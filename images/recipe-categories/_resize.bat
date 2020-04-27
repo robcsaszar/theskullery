b182fcd8749b4d7576d30a3fd7a72ecaf37c5577
Robert Csaszar <robert@nhg.design>
2020-04-27 19:32:19 +0300

    added favorite button

--- images/recipe-categories/_resize.bat
+++
@@ -1,6 +0,0 @@
-for /d /r %%aa in (*) do (
-  if exist "%%~fi\%%~ni.jpg" (
-cd ..\..\_scale
-    call scale.bat -source "%%~fi\%%~ni.jpg" -target "%%~fi\%%~ni.jpg" -max-width 1080 -force yes
-  )
-)
