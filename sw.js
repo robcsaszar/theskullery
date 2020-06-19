08e3b788ddd92d16c3d7c46c611c9dccf38ea04b
Robert Csaszar <robert@nhg.design>
2020-06-20 02:22:34 +0300

    trying out some more PWA thing

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('https://unpkg.com/workbox-runtime-caching@1.3.0/build/importScrip
 importScripts('https://unpkg.com/workbox-routing@1.3.0/build/importScripts/workbox-routing.prod.v1.3.0.js');
 
 const assetRoute = new workbox.routing.RegExpRoute({
-    regExp: new RegExp('^http://localhost:4000/*'),
+    regExp: new RegExp('/*'),
     handler: new workbox.runtimeCaching.CacheFirst()
 });
 
@@ -13,3 +13,4 @@ router.registerRoutes({routes: [assetRoute]});
 router.setDefaultHandler({
     handler: new workbox.runtimeCaching.CacheFirst()
 });
+
